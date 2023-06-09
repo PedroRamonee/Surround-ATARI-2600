347d9ecd7b07ff2fb5c47749ac71b1335c39199c
asuka <118109383+setinhaw@users.noreply.github.com>
2023-06-09 10:38:11 -0300

    Add files via upload
    menuzinho iniciando

--- main.cpp
+++ main.cpp
@@ -1,3 +1,36 @@
+<<<<<<< HEAD
+#include "game.hpp"
+
+int main() {
+  RenderWindow window(VideoMode(700, 700), "Surround RAMAKE");
+
+  Snake cobra(&window, Color::Blue);
+
+  Event event;
+
+  while (window.isOpen()) {
+    while (window.pollEvent(event)) {
+      {
+        if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape)) window.close();
+      }
+    }
+
+  window.clear();
+ 
+ //aqui
+
+  window.clear();
+
+    cobra.changePosition(&window);
+
+    window.clear();
+    cobra.render(&window);
+    window.display();
+    sleep(milliseconds(10.0f));
+  }
+  return 0 ;
+}
+=======
 #include "game.hpp"
 
 int main() {
@@ -23,3 +56,4 @@ int main() {
   }
   return 0 ;
 }
+>>>>>>> 296c4776d8de8e27aa01b049f7c62e361ecb7b1d
---
+++ menu.hpp
@@ -0,0 +1,52 @@
+#ifndef MENU_HPP_
+#define MENU_HPP_
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+
+class menu{
+    private:
+    
+    Image icon;
+    Image background;
+    float positionx=0;
+    float positiony=0;
+    Music start;
+    Music music;
+    Font fonte;
+
+    public:
+    menu(RenderWindow *window){
+
+        /*
+     Construtor do Objeto Menu(-)
+
+     - Configura a posição x e y inicial
+     - Carrega imagens de background/icone
+     - Carrega arquivos de música de music/start
+     - Carrega a fonte
+
+     */
+    this->icon = icon.loadFromFile("icone.png");
+
+    this->background = background.loadFromFile("background.jpg");
+
+    this->positionx = window->getSize().x;
+    this->positiony = window->getSize().y;
+
+    this->start = start.openFromFile("startdojogo.wav");
+
+    this->music = music.openFromFile("menumusic.wav");
+
+    this->fonte = fonte.loadFromFile("design.graffiti.comicsansmsgras.ttf");
+    }
+};
+
+
+
+#endif /* MENU_HPP_ */
